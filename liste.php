ede86b52879b9527c1b01dc9e3ce4bd0744e4442
m6bnt <123626405+m6bnt@users.noreply.github.com>
2023-03-01 15:08:56 +0100

    coucou

---
+++ liste.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<h1>Liste des films</h1>
+
+
+<?php
+//Lister le contenu de la table movies
+
+//1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20205722_cnam', 'id20205722_mehdi', 'J3@fy?1UpR5Rp#Ez');
+$base->exec("SET CHARACTER SET utf8");
+
+//2° - Préparation de requette et execution
+$retour = $base->query('SELECT * FROM movies;');
+
+//3° - Lecture du resultat de la requette
+while ($data = $retour->fetch()){
+echo $data['id']." ".$data['titre']." ".$data['genre']." ".$data['annee']."</br>";
+}
+
+?>
+
+</body>
+</html>
