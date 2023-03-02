e8c1ca100d2401d06496fb88daab8e2d3371d40e
m6bnt <123626405+m6bnt@users.noreply.github.com>
2023-03-02 16:32:56 +0100

    okok

--- POI.php
+++ POI.php
@@ -1,20 +1,21 @@
 <?php
-
+$long= $_GET['long'];
+$lat= $_GET['lat'];
 //C'est le POI de l'utilisateur hhh
 echo "lat\tlon\ttitle\tdescription\ticon\ticonSize\ticonOffset\n";
-echo "43.296482\t5.36978\tMoi\tMa Position\tluffy.png\t24,24\t0,-24\n";
+echo "$lat\$long\tMoi\tMa Position\tluffy.png\t24,24\t0,-24\n";
 
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20205722_cnam', 'id20205722_mehdi', 'whV(U[WTF#ni1cY$');
 $base->exec("SET CHARACTER SET utf8");
 
 //2° - Préparation de requette et execution
-$retour = $base->query('SELECT *, get_distance_metres(\'43.296482\', \'5.36978\', equi_lat, equi_long) 
+$retour = $base->query("SELECT *, get_distance_metres('$lat', '$long', equi_lat, equi_long) 
 AS proximite 
 FROM equipement 
 HAVING proximite < 1000 ORDER BY proximite ASC
 LIMIT 10;
-');
+");
 
 //Boucle For
 while ($data = $retour->fetch()){
--- lacarte.php
+++ lacarte.php
@@ -1,18 +1,22 @@
 <html><body>
   <div id="mapdiv"></div>
   <script src="https://openlayers.org/api/OpenLayers.js"></script>
+  <?php
+  $long= $_GET['long'];
+  $lat= $_GET['lat'];
+  ?>
   <script>
     map = new OpenLayers.Map("mapdiv");
     map.addLayer(new OpenLayers.Layer.OSM());
-       
+    var url = "./POI.php?long="+"<?php echo $long?>"+"&lat="+"<?php echo $lat?>";
     var pois = new OpenLayers.Layer.Text( "My Points",
-                    { location:"./POI.php",
+                    { location:url,
                       projection: map.displayProjection
                     });
     map.addLayer(pois);
  
     //Set start centrepoint and zoom hhh   
-    var lonLat = new OpenLayers.LonLat(5.36978,43.296482)
+    var lonLat = new OpenLayers.LonLat("<?php echo $long?>","<?php echo $lat?>")
           .transform(
             new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
             map.getProjectionObject() // to Spherical Mercator Projection
