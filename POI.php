2811d350c3a00487cf4827479c9866843452a54e
m6bnt <123626405+m6bnt@users.noreply.github.com>
2023-03-02 16:38:36 +0100

    all

--- POI.php
+++ POI.php
@@ -9,7 +9,7 @@ echo "$lat\$long\tMoi\tMa Position\tluffy.png\t24,24\t0,-24\n";
 $base = new PDO('mysql:host=localhost; dbname=id20205722_cnam', 'id20205722_mehdi', 'whV(U[WTF#ni1cY$');
 $base->exec("SET CHARACTER SET utf8");
 
-//2° - Préparation de requette et execution
+//2° - Préparation de requette et execution hhh
 $retour = $base->query("SELECT *, get_distance_metres('$lat', '$long', equi_lat, equi_long) 
 AS proximite 
 FROM equipement 
